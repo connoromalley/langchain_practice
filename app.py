cab09283d4445246ab870d9765c470f405c9ab58
ConnorOMalley <wcomalley@aol.com>
2023-09-26 20:03:10 -0600

    why isnt this working

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from apikey import apikey
 
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
 
 os.environ['OPENAI_API_KEY'] = apikey
 
@@ -10,12 +12,29 @@ os.environ['OPENAI_API_KEY'] = apikey
 st.title('🦜🔗 Connor\'s AI')
 prompt = st.text_input('Enter prompt here')
 
+# Promte templates
+title_template = PromptTemplate(
+    input_variable = ['topic'],
+    template = 'write me a youtube video title about {topic}'
+)
+
+# Script templates
+script_template = PromptTemplate(
+    input_variable = ['title'],
+    template = 'write me a youtube video script based on this title TITLE: {title}'
+)
+
 # LLMs
 llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose = True, output_key = 'title')
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose = True, output_key='script')
+sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], 
+                                   output_variables=['title', 'script'], verbose = True)
 
 # Show stuff to the screen if there's a prompt
 if prompt:
-    response = llm(prompt)
-    st.write(response)
+    response = sequential_chain({'topic':prompt})
+    st.write(response['title'])
+    st.write(response['script'])
 
 #test commit 
